1d599812ecbdf858d1a1c0aac0633d39aae2b5e2
didjoman <alexrupp@free.fr>
2014-11-16 18:53:10 +0100

    (cleaning) creation d'un fichier pour les files de processus + un fichier pour les processus

--- src_de_base/scheduler_coop.c
+++ src_de_base/scheduler_coop.c
@@ -1,5 +1,4 @@
 #include "scheduler_coop.h"
-#include <malloc.c.h>
 
 // *** Static Function Definition : 
 static int32_t mon_pid(void);
@@ -9,13 +8,7 @@ extern void ctx_sw(int32_t* a, int32_t* b);
 static void load_elected();
 static void unload_elected();
 
-static void push(struct proc_queue* queue, struct Process* p);
-static struct Process* pop(struct proc_queue* queue);
-static bool empty(struct proc_queue* queue);
-
-
 // *** Global Variables :
-//struct Process* proc_table[PROC_NB];
 struct proc_queue activables;
 int32_t last_proc_id = -1;
 struct Process* elected_proc;
@@ -75,7 +68,6 @@ static char* mon_nom(void)
 */
 void ordonnance(void)
 {
-
 	// We set the statue of the current elected proc to ACTIVABLE
 	struct Process* former_elected = elected_proc;
 	// Else the current elected returns to the list of activable processes
@@ -94,21 +86,7 @@ int32_t cree_processus(void (*code)(void), char *nom)
 		return -1;
 
 	// If possible we create the process : 
-	++last_proc_id;
-
-        struct Process* proc = (struct Process*) malloc(sizeof(struct Process));
-        proc->pid = last_proc_id;
-        strncpy(proc->name, nom, NAME_SIZE);
-	proc->state = ACTIVABLE;
-
-        // At the first execution the context of proc1 has never been saved.
-        // |- We need to store a pointer on the stack in the register save zone
-        proc->save_zone[ESP_REG_ID] =
-                (int32_t)(proc->stack+(STACK_SIZE - 1));
-        // |- The stack normally stocks the adress of the function that were
-        // |  running before the interruption of the process :
-        proc->stack[STACK_SIZE - 1] = (int32_t)code;
-        // |- Rq : the stack is fulfilled from the higher index
+        struct Process* proc = new_process(++last_proc_id, ACTIVABLE, code, nom);
 	
 	// We add the new process to the list of activables porcesses
 	push(&activables, proc);
@@ -142,58 +120,4 @@ static void unload_elected()
 
 	elected_proc->state = ACTIVABLE;
 	push(&activables, elected_proc);
-}
-
-/******************************************************************************/
-/*************************** DATA STRUCTURE : QUEUE ***************************/
-/******************************************************************************/
-
-// TODO : put it in an other file;
-
-/**
- * Add Process in tail
- * @param queue is the queue we want to add the process to.
- * @param p is a pointer to the process to insert on the queue 
-*/
-static void push(struct proc_queue* queue, struct Process* p)
-{
-	// If the queue is empty, the new proc will be the first and last one
-	if(empty(queue)){
-		queue->head = p;
-		queue->tail = p;
-	} else {
-		// Insert the process in tail.
-		queue->tail->next = p;
-		queue->tail = queue->tail->next;
-	}
-	// We make sure that the process has no next proc, since it is the last.
-	p->next = NULL;
-}
-
-/**
- * Pop the first element of the queue
- * @param queue is the queue we want to add the process to.
-*/
-static struct Process* pop(struct proc_queue* queue)
-{
-	struct Process* tmp = queue->head;
-	// Update the new head of the list;
-	if(!empty(queue))
-		queue->head = queue->head->next;
-
-	// The process is removed from the list, so it has next proc any more
-	if(tmp != NULL)
-		tmp->next = NULL;
-
-	return tmp;
-}
-
-
-/**
- * @param queue is the queue we want to add the process to.
- * @return true if the queue is empty, false else.
-*/
-static bool empty(struct proc_queue* queue)
-{
-	return queue->head == NULL;
 }
--- src_de_base/scheduler_coop.h
+++ src_de_base/scheduler_coop.h
@@ -6,31 +6,10 @@
 #include <stdbool.h>
 #include <inttypes.h>
 #include <cpu.h>
+#include "process_queue.h"
+#include "process.h"
 
-
-#define PROC_NB 1000
-#define STACK_SIZE 512
-#define NAME_SIZE 20
-#define SAVE_ZONE_SIZE 5
 #define PID_MAX 1000
-#define ESP_REG_ID 1
-
-enum state{ELU, ACTIVABLE};
-
-struct Process {
-	int pid;
-	char name[NAME_SIZE];
-	enum state state;
-	int32_t save_zone[SAVE_ZONE_SIZE];
-	int32_t stack[STACK_SIZE];
-	struct Process* next;
-};
-
-struct proc_queue{
-        struct Process* head;
-        struct Process* tail;
-};
-
 
 void idle(void);
 void proc1(void);
